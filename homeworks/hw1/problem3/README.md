dd2dd76b6ceb088853ef50c1f41a45efa4748201
Benji Meiner <bnmeiner@gmail.com>
2020-09-13 20:35:40 -0400

    added problem3

---
+++ homeworks/hw1/problem3/README.md
@@ -0,0 +1,18 @@
+How to compile the Program:
+	type: ln -s makefile.3 makefile
+	type: make
+
+How to run the program:
+	type: ./problem3
+
+Output from the program:
+
+this is a test a
+this is a test b
+this is a test e
+his is a test f
+this is a test n
+
+this is a test r
+this is a test t
+this is a test v
---
+++ homeworks/hw1/problem3/problem3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void main() {
+	printf("this is a test a\a");
+	printf("\n");
+	printf("this is a test b\b");
+	printf("\n");
+	//printf("this is a test\c");
+	//printf("\n");
+	//printf("this is a test\d");
+	//printf("\n");
+	printf("this is a test e\e");
+	printf("\n");
+	printf("this is a test f\f");
+	printf("\n");
+	//printf("this is a test\g");
+	//printf("\n");
+	//printf("this is a test\h");
+	//printf("\n");
+	//printf("this is a test\i");
+	//printf("\n");
+	//printf("this is a test\j");
+	//printf("\n");
+	//printf("this is a test\k");
+	//printf("\n");
+	//printf("this is a test\l");
+	//printf("\n");
+	//printf("this is a test\m");
+	//printf("\n");
+	printf("this is a test n\n"); 
+	printf("\n");
+	//printf("this is a test\o"); 
+	//printf("\n");
+	//printf("this is a test\p"); 
+	//printf("\n");
+	//printf("this is a test\q"); 
+	//printf("\n");
+	printf("this is a test r\r"); 
+	printf("\n");
+	//printf("this is a test\s"); 
+	//printf("\n");
+	printf("this is a test t\t"); 
+	printf("\n");
+	printf("this is a test v\v"); 
+	printf("\n");
+	//printf("this is a test\w"); 
+	//printf("\n");
+	//printf("this is a test\x"); 
+//	printf("\n");
+//	printf("this is a test\y"); 
+	//printf("\n");
+	//printf("this is a test\z");  
+	//printf("\n");
+		 
+
+}
