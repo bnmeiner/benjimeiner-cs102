a190a1ffe177c83c1cc9e9223302a856e9065343
Benji Meiner <bnmeiner@gmail.com>
2020-09-13 20:10:04 -0400

    added problem2

---
+++ homeworks/hw1/problem2/problem2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int cnt=0, limit=2;
+
+	while (cnt <= limit){
+		printf("It's fun to have fun but you have to know how\n-Dr. Seuss\n");
+		cnt = cnt + 1;
+	}	
+}
